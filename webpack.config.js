20d5498574590d454aab78426608de0b292eac23
Pratihar, Gagandeep @ Gurgaon <Gagandeep.Pratihar@cbre.com>
2018-10-23 12:08:17 +0530

    webpack file changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,63 @@
-var path = require('path');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public'),
+        contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 9000
-      },
+    },
     module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-            test: /\.html$/,
-            use: [
-              {
-                loader: "html-loader"
-              }
+        rules: [{
+                test: /\.js$/, //for js and jsx files
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/, //for html
+                use: [{
+                    loader: "html-loader"
+                }]
+            },
+            {
+                test: /\.css$/, //for css
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|png|svg|jpg|gif)$/, //for images and fonts
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+             new CleanWebpackPlugin(['dist']),
+             new HtmlWebpackPlugin({
+                title: 'Output Management'
+              })
             ]
-        },
-        {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-        },
-        {
-            test: /\.svg$/,
-            loader: 'svg-inline-loader'
-        }
-      ]
-    }
-  };
+};
